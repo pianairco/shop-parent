2dc2b54db82a08a79fb322851d4ed3f21cdc177c
Mohammad Javad Rahmati <mj.rahmati@rha.local>
2020-05-21 13:27:17 +0430

    add control-image

--- component-parent/form-creator/src/test/java/ir/piana/business/formcreator/cfg/FormSaver.java
+++ component-parent/form-creator/src/test/java/ir/piana/business/formcreator/cfg/FormSaver.java
@@ -1,22 +1,13 @@
 package ir.piana.business.formcreator.cfg;
 
 import ir.piana.dev.springvue.core.action.Action;
-import net.sf.jmimemagic.*;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import javax.activation.MimetypesFileTypeMap;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLConnection;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service("FormSaver")
 public class FormSaver extends Action {
